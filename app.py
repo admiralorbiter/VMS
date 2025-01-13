5fa0f14276c2e832e15ecd4a19f88127f28826ba
Jonathan Lane <admiralorbiter@gmail.com>
2025-01-13 10:45:29 -0600

    trying to fix login in production

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ else:
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'  # Redirect to 'login' view if unauthorized
+login_manager.login_view = 'auth.login'  # Redirect to 'login' view if unauthorized
 login_manager.login_message_category = 'info'
 
 # Create the database tables
